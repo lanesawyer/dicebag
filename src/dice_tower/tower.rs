ad9ebd8c2cb04d072fa123a0a2448b115f00e1af
Lane Sawyer <github@lanesawyer.dev>
2021-05-31 22:21:51 -0700

    Some more dice

--- src/dice_tower/tower.rs
+++ src/dice_tower/tower.rs
@@ -1,11 +1,17 @@
 use std::num::NonZeroU8;
-
 use yew::{html, Component, ComponentLink, Html, ShouldRender};
+use yew::services::ConsoleService;
 
 use crate::dice_tower::Roll;
 
 pub enum Msg {
+    RollD4,
+    RollD6,
+    RollD8,
     RollD10,
+    RollD12,
+    RollD20,
+    RollD100,
 }
 
 pub struct Tower {
@@ -20,7 +26,24 @@ impl Component for Tower {
         Self { link }
     }
 
-    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+    fn update(&mut self, msg: Self::Message) -> ShouldRender {
+        let (number, dice) = match msg {
+            Msg::RollD4 => (NonZeroU8::new(1).unwrap(), crate::dice_tower::DiceType::D4),
+            Msg::RollD6 => (NonZeroU8::new(1).unwrap(), crate::dice_tower::DiceType::D6),
+            Msg::RollD8 => (NonZeroU8::new(1).unwrap(), crate::dice_tower::DiceType::D8),
+            Msg::RollD10 => (NonZeroU8::new(1).unwrap(), crate::dice_tower::DiceType::D10),
+            Msg::RollD12 => (NonZeroU8::new(1).unwrap(), crate::dice_tower::DiceType::D12),
+            Msg::RollD20 => (NonZeroU8::new(1).unwrap(), crate::dice_tower::DiceType::D20),
+            Msg::RollD100 => (NonZeroU8::new(1).unwrap(), crate::dice_tower::DiceType::D100),
+        };
+
+        let result = Roll::roll(&Roll {
+            number,
+            dice
+        });
+
+        ConsoleService::log(&format!("{:?}", result));
+
         true
     }
 
@@ -29,11 +52,24 @@ impl Component for Tower {
     }
 
     fn view(&self) -> Html {
+        let d4_click = self.link.callback(|_| Msg::RollD4);
+        let d6_click = self.link.callback(|_| Msg::RollD6);
+        let d8_click = self.link.callback(|_| Msg::RollD8);
         let d10_click = self.link.callback(|_| Msg::RollD10);
+        let d12_click = self.link.callback(|_| Msg::RollD12);
+        let d20_click = self.link.callback(|_| Msg::RollD20);
+        let d100_click = self.link.callback(|_| Msg::RollD100);
+
         html! {
             <section id="dice-tower" class="text-block">
                 <h3>{ "Dice Tower" }</h3>
-                <button onclick=d10_click>{ "Roll 1 D10" }</button>
+                <button onclick=d4_click>{ "D4" }</button>
+                <button onclick=d6_click>{ "D6" }</button>
+                <button onclick=d8_click>{ "D8" }</button>
+                <button onclick=d10_click>{ "D10" }</button>
+                <button onclick=d12_click>{ "D12" }</button>
+                <button onclick=d20_click>{ "D20" }</button>
+                <button onclick=d100_click>{ "D100" }</button>
                 <div>{ Roll::roll(&Roll {
                     number: NonZeroU8::new(1).unwrap(),
                     dice: crate::dice_tower::DiceType::D10,
